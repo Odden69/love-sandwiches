29173f3335312478e1b0fd7110477886138e640d
Odden69 <helena.joh2@gmail.com>
2021-11-20 15:53:42 +0000

    Add stock value rekommendation

--- run.py
+++ run.py
@@ -122,8 +122,22 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("\nWelcome to Love Sandwiches Data Automation\n")
-main()
+stock_data = main()
 
+def get_stock_values(data):
+    """
+    Get sandwich types from stock worksheet
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    stock_values = {headings[i]: data[i] for i in range(len(headings))}
+
+    return stock_values
+
+stock_values = get_stock_values(stock_data)
+print('Make the following numbers of sandwiches for next market:\n')
+print(stock_values)
